285dd05dedeb0e753f76ead6fa9345c883f82552
agentzh (章亦春) <agentzh@gmail.com>
2010-03-30 15:06:54 +0800

    a lot of progress.

--- Cheater.pm
+++ Cheater.pm
@@ -4,24 +4,141 @@ use strict;
 use warnings;
 
 use String::Random;
+use JSON::Syck;
+
+use Carp qw( croak );
+
 use base 'Exporter';
 
 our @EXPORT = qw(
-    view is like line run_view write_php
+    seq view is like line
+    run_view write_php
+    json
+    regex
 );
 
+our %Views;
+
+sub apply_pattern ($$$);
+sub generate_row ($$$);
+
 sub view ($$@) {
+    my $name = shift;
+    my %params = @_;
+
+    for my $key (keys %params) {
+        if ($key eq 'cols' || $key eq 'ensure') {
+            next;
+        }
+        croak "Invalid key in view definition: $key";
+    }
+
+    $Views{$name} = \%params;
 }
 
-sub is (@) {
+sub seq (@) {
+    return ['seq', @_];
 }
 
 sub line ($$) {
+    return ['line', @_];
 }
 
 sub run_view ($$) {
+    my ($name, $n) = @_;
+
+    my $view = $Views{$name};
+
+    if (!defined $view) {
+        croak "View \"$name\" not defined";
+    }
+
+    my $cols = $view->{cols};
+    my $ensure = $view->{ensure};
+
+    my $ctx = {};
+
+    my @rows;
+    for (1..$n) {
+        my $row = generate_row($ctx, $cols, $ensure);
+        unless ($ensure->($row)) {
+            redo;
+        }
+
+        push @rows, $row;
+    }
+
+    return \@rows;
+}
+
+sub generate_row ($$$) {
+    my ($ctx, $cols, $ensure) = @_;
+
+    my $row = {};
+
+    while (my ($col, $pat) = each %$cols) {
+        $row->{$col} = apply_pattern($ctx, $col, $pat);
+    }
+
+    return $row;
+}
+
+sub apply_pattern ($$$) {
+    my ($total_ctx, $col, $pat) = @_;
+
+    my $ctx = ($total_ctx->{$col} ||= {});
+
+    my @args = @$pat;
+    my $op = shift @args;
+
+    if ($op eq 'seq') {
+        my $i = ($ctx->{i} ||= 0);
+
+        #warn $i;
+
+        $ctx->{i}++;
+
+        if ($i >= @args) {
+            return $args[-1];
+        }
+
+        my $val = $args[$i];
+
+        if (ref $val) {
+            my $sub_ctx = ($ctx->{ctx} ||= {});
+            return apply_pattern($sub_ctx, $col, $val);
+        }
+
+        return $val;
+    }
+
+    if ($op eq 'line') {
+        my ($from, $to) = @args;
+
+        if (!defined $ctx->{prev}) {
+            $ctx->{prev} = int(rand($to - $from) + $from);
+        }
+
+        my $k = 1 + (rand(0.3) - 0.15);
+
+        my $cur = int($ctx->{prev} * $k);
+
+        $ctx->{prev} = $cur;
+
+        return $cur;
+    }
 }
 
 sub write_php ($$@) {
 }
 
+sub json ($) {
+    my $data = shift;
+    JSON::Syck::Dump($data);
+}
+
+sub regex ($) {
+    return ['regex', @_];
+}
+
+1;
--- shopflow.pl
+++ shopflow.pl
@@ -7,27 +7,30 @@ use Cheater;
 
 view shopflow =>
     cols => {
-        hour => is(0..23),
-        uv => line(0, 16000),
+        hour => seq(0..23),
         pv => line(0, 16000),
+        uv => line(0, 16000),
     },
     ensure => sub {
+        my $r = shift;
+        $r->{uv} <= $r->{pv};
     };
 
 my $data = run_view shopflow => 24;
+warn json($data);
 
 write_php 'view-shopflow.php',
     json($data),
     when => {
-        day => like('[02468]$'),
+        day => regex('[02468]$'),
     };
 
 write_php 'batch-shopflow.php',
     json([
-        run_view shopflow => 24,
-        run_view shopflow => 24,
-    ),
+        run_view(shopflow => 24),
+        run_view(shopflow => 24),
+    ]),
     when => {
-        days => like('[02468],'),
+        days => regex('[02468],'),
     };
 
