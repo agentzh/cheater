349d169261ee8305fc72fb8043944a1ed414669f
agentzh (章亦春) <agentzh@gmail.com>
2010-03-30 12:06:56 +0800

    initial checkin.

---
+++ Cheater.pm
@@ -0,0 +1,27 @@
+package Cheater;
+
+use strict;
+use warnings;
+
+use String::Random;
+use base 'Exporter';
+
+our @EXPORT = qw(
+    view is like line run_view write_php
+);
+
+sub view ($$@) {
+}
+
+sub is (@) {
+}
+
+sub line ($$) {
+}
+
+sub run_view ($$) {
+}
+
+sub write_php ($$@) {
+}
+
